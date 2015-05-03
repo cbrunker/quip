46147a0fde2e511e0c40d357ceea9ae7afb6a6f1
cbrunker <capel@ideabite.org>
2015-05-03 15:06:21 +0930

    FIX: Ensure quoted path for openssl.exe location (Windows). ADD: Ubuntu 15.04 instructions

--- Resources/create_certs_windows.bat
+++ Resources/create_certs_windows.bat
@@ -2,8 +2,8 @@
 
 set /p executableLocation=<openssl.cfg
 
-%executableLocation%\openssl.exe ecparam -genkey -name secp256k1 -out server.key.orig
+"%executableLocation%\openssl.exe" ecparam -genkey -name secp256k1 -out server.key.orig
 
-%executableLocation%\openssl.exe req -new -key server.key.orig -out server.csr -subj "/"
+"%executableLocation%\openssl.exe" req -new -key server.key.orig -out server.csr -subj "/"
 
-%executableLocation%\openssl.exe req -x509 -days 365 -key server.key.orig -in server.csr -out server.crt
+"%executableLocation%\openssl.exe" req -x509 -days 365 -key server.key.orig -in server.csr -out server.crt
